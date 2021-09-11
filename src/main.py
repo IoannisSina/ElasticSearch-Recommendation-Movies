c0dd055aaec24003a038a2281f06399d979a16c0
sina_giannis <49285637+sinakhs@users.noreply.github.com>
2021-09-11 16:27:05 +0300

    Insert movies and search by given title

--- src/main.py
+++ src/main.py
@@ -1,30 +1,15 @@
+from script1 import insert_movies, search_movies
 from elasticsearch import Elasticsearch
-es = Elasticsearch()
 
-es.indices.create(index="first_index")
-print(es.indices.exists(index="first_index"))
+es = Elasticsearch(HOST="http://localhost", PORT="9200")
 
+# insert movies
+# insert_movies(es)
 
+# search for a movie
+title_input = "Lady" # input("Please enter a movie title:")
+response = search_movies(es, title_input)
+print("\n\nMovies similar to " + title_input + " (descending order, BM25 metric):\n")
+for movie in response['hits']['hits']:
+  print(movie['_source']['title'] + " with a similarity score of " + str(movie['_score']))
 
-
-
-
-"""
-{
-  "name" : "DESKTOP-0QIB0R2",
-  "cluster_name" : "elasticsearch",
-  "cluster_uuid" : "VPPzbUQ4RZSXLXJevdj3ig",
-  "version" : {
-    "number" : "7.14.1",
-    "build_flavor" : "default",
-    "build_type" : "zip",
-    "build_hash" : "66b55ebfa59c92c15db3f69a335d500018b3331e",
-    "build_date" : "2021-08-26T09:01:05.390870785Z",
-    "build_snapshot" : false,
-    "lucene_version" : "8.9.0",
-    "minimum_wire_compatibility_version" : "6.8.0",
-    "minimum_index_compatibility_version" : "6.0.0-beta1"
-  },
-  "tagline" : "You Know, for Search"
-}
-"""
--- src/script1.py
+++ src/script1.py
@@ -1,4 +1,75 @@
 """
 Read movies.csv and add the to Elasticsearch DB
 """
-from elasticsearch import Elasticsearch
+import os
+from elasticsearch import Elasticsearch, helpers
+import pandas as pd
+
+def bulk_json_data(movies, _index):
+    for rec in movies:
+        yield {
+            "_index": _index,
+            "_id": rec["movieId"],
+            "_source": rec,
+        }
+
+def insert_movies(es):
+    """
+    Index all movies with one bulk json file
+    """
+    #Creating index, ignore if already exists
+    es.indices.create(index='movies', ignore=400,body={
+        "settings":{
+            "index":{
+                "similarity":{
+                    "default":{
+                        "type":"BM25"
+                    }
+                }
+            }
+        },
+        "mappings":{
+            "standard_mapping":
+            {
+                "title":{"analyzer":"english"}
+                }
+            }
+        }
+    )
+    movies = pd.read_csv(os.path.abspath(os.getcwd()) + "\src\datasets\movies.csv", index_col=False).to_dict('records') # movies as records
+
+    try:
+        # bulk call for indexing all movies
+        response = helpers.bulk(es,bulk_json_data(movies, 'movies'))
+        assert response[0] == len(movies), "Error: Not all movies were inserted succesfully."
+        # print(response)
+    except Exception as e:
+        print("Error: ", e)
+    # es.indices.delete(index='movies')
+
+def search_movies(es, title):
+    """
+    return all movies matching with the given string
+    """
+
+    query = {
+        # get only first 10 records
+        "size": 10,
+        "query": {
+            "match": {
+                "title": title
+            }
+        },
+        "sort": [
+            {"_score": {"order": "desc"}}
+        ]
+    }
+
+    result = es.search(body=query, index='movies')
+    return result
+
+if __name__ == "__main__":
+    es = Elasticsearch(HOST="http://locahost", PORT=9200)
+    # insert_movies(es)
+    response = search_movies(es, "Toy Story")
+    print(response)
